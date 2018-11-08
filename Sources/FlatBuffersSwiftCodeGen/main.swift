5f1b060b2f9363cd3c2dab83b65faad7fda5894e
Toby O'Connell <toby.oconnell@music-group.com>
2018-11-08 15:47:05 +0000

    Modify command line arguments to take directories
    This will recursively traverse subdirectories of the input directory, convert fbs files to swift files and write to the output directory, mirroring the input directory structure

--- Sources/FlatBuffersSwiftCodeGen/main.swift
+++ Sources/FlatBuffersSwiftCodeGen/main.swift
@@ -9,55 +9,77 @@
 import Foundation
 import FlatBuffersSwiftCodeGenCore
 
-if CommandLine.arguments.count < 2 {
-    print("⚠️ Please provide path to .fbs file as first argument")
+ let arguments = Array(CommandLine.arguments.dropFirst()) // The first argument is this executable
+
+if !(2 ... 3 ~= arguments.count) {
+    print("⚠️ Wrong number of arguments provided. Expected (2...3), received \(arguments.count)")
     exit(1)
 }
 
-let fbsPath = CommandLine.arguments[1]
-let fbsUrl = URL(fileURLWithPath: fbsPath)
+let inputURL = URL(fileURLWithPath: arguments[0])
+let outputURL = URL(fileURLWithPath: arguments[1])
 
-guard let fileContent = try?Data(contentsOf: fbsUrl) else {
-    print("⚠️ Could not read content of the .fbs file \(fbsUrl)")
+let withoutImport = arguments.count == 3 &&
+    (arguments[2] == "download" || arguments[2] == "noImport")
+
+
+let getURLs = {
+    return FileManager.default.enumerator(at: inputURL,
+                                          includingPropertiesForKeys: [],
+                                          options: .skipsHiddenFiles)
+}
+
+guard let urlEnumerator = getURLs() else {
+    print("⚠️ Could not get .fbs files from path \(inputURL)")
     exit(1)
 }
 
+let fbsURLs = (urlEnumerator.allObjects as! [URL])
+    .filter { $0.pathExtension == "fbs"}
 
-
-if CommandLine.arguments.count < 3 {
-    print("⚠️ Please provide path to .swift file (which you want to generate) as second argument")
-    exit(1)
+extension Array where Element == URL {
+    func schemas() -> [(url: URL, schema: Schema)] {
+        return self
+            .compactMap { url -> (url: URL, data: Data)? in
+                let data = try? Data(contentsOf: url)
+                return data == nil ? nil : (url, data!)
+            }
+            .compactMap { (url: URL, data: Data) -> (url: URL, schema: Schema)? in
+                let schema = data.withUnsafeBytes { (p: UnsafePointer<UInt8>) -> Schema? in
+                    return Schema.with(pointer: p, length: data.count)?.0
+                }
+                return schema == nil ? nil : (url, schema!)
+            }
+    }
 }
 
-let swiftPath = CommandLine.arguments[2]
-let swiftUrl = URL(fileURLWithPath: swiftPath)
+let swiftFileContents = fbsURLs
+    .schemas()
+    .compactMap { (url: URL, schema: Schema) -> (url: URL, data: Data)? in
+        let data = schema.swift(withImport: !withoutImport).data(using: .utf8)
+        return data == nil ? nil : (url, data!)
+    }
 
-print("Generating: \(fbsPath) -> \(swiftPath)")
-
-
-
-let schema = fileContent.withUnsafeBytes { (p: UnsafePointer<UInt8>) -> Schema? in
-    return Schema.with(pointer: p, length: fileContent.count)?.0
+for swiftFileContent in swiftFileContents {
+    let suffix = swiftFileContent.url.path.replacingOccurrences(of: inputURL.path + "/", with: "")
+    let newURL = outputURL.appendingPathComponent(suffix).deletingPathExtension().appendingPathExtension("swift")
+    
+    do {
+        try FileManager.default.createDirectory(at: newURL.deletingLastPathComponent(), withIntermediateDirectories: true)
+        try swiftFileContent.data.write(to: newURL)
+        print("✅ Wrote swift file to \(newURL.path)")
+    } catch(let error) {
+        print("❌ Could not generate \(newURL.path), (\(error.localizedDescription)")
+    }
 }
 
-let withoutImport = CommandLine.arguments.count > 3
-                    && (CommandLine.arguments[3] == "download" || CommandLine.arguments[3] == "noImport")
-
-if let swiftFileContent = schema?.swift(withImport: !withoutImport) {
-    try!swiftFileContent.data(using: .utf8)?.write(to: swiftUrl)
-    print("✅ Completed")
-} else {
-    print("❌ Could not generate")
-}
-
-if CommandLine.arguments.count > 3
-    && CommandLine.arguments[3] == "download" {
+if arguments.count == 3 && arguments[2] == "download" {
     print("🕐 Downloading FlatBuffersBuilder")
     let builderData = try Data(contentsOf: URL(string: "https://raw.github.com/mzaks/FlatBuffersSwift/1.0.0/FlatBuffersSwift/FlatBuffersBuilder.swift")!)
-    try!builderData.write(to: swiftUrl.deletingLastPathComponent().appendingPathComponent("FlatBuffersBuilder.swift"))
+    try!builderData.write(to: outputURL.deletingLastPathComponent().appendingPathComponent("FlatBuffersBuilder.swift"))
     print("✅ Completed")
     print("🕐 Downloading FlatBuffersReader")
     let readerData = try Data(contentsOf: URL(string: "https://raw.githubusercontent.com/mzaks/FlatBuffersSwift/1.0.0/FlatBuffersSwift/FlatBuffersReader.swift")!)
-    try!readerData.write(to: swiftUrl.deletingLastPathComponent().appendingPathComponent("FlatBuffersReader.swift"))
+    try!readerData.write(to: outputURL.deletingLastPathComponent().appendingPathComponent("FlatBuffersReader.swift"))
     print("✅ Completed")
 }
